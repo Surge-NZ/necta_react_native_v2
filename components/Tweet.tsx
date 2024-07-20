78cfe881acc194d9e76b85d44d08aad84b085672
Michael <michael@surgeconsulting.co.nz>
2024-07-20 20:47:43 +1200

    accurate prop types for tweet

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,24 +1,8 @@
 import { StyleSheet, Image } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { TweetProps } from '@/types';
 
-type TweetProps = {
-  tweet: {
-    id: string;
-    user: {
-      id: string;
-      name: string;
-      username: string;
-      image: string;
-    };
-    createdAt: string;
-    content: string;
-    image?: string;
-    numberOfComments: number;
-    numberOfRetweets: number;
-    numberOfLikes: number;
-  };
-};
 
 const Tweet = ({ tweet }: TweetProps) => {
   return (
---
+++ types/index.ts
@@ -0,0 +1,17 @@
+export type TweetProps = {
+    tweet: {
+      id: string;
+      user: {
+        id: string;
+        name: string;
+        username: string;
+        image: string;
+      };
+      createdAt: string;
+      content: string;
+      image?: string;
+      numberOfComments: number;
+      numberOfRetweets: number;
+      numberOfLikes: number;
+    };
+  };
